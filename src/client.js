1a1668281dfc059555fde81376eea1aa0f5543b9
frankpagan <frank@cocreate.app>
2023-09-07 13:18:31 -0600

    test

--- src/client.js
+++ src/client.js
@@ -1,13 +1,13 @@
 import api from '@cocreate/api'
 
 const CoCreateLinkedin = {
-	name: 'linkedin',
-	endPoints: {
-		getLinkedinProfile: {},
-		publishPost: {},
-		updatePost: {},
-		deletePost: {}
-	},	
+    name: 'linkedin',
+    endPoints: {
+        getLinkedinProfile: {},
+        publishPost: {},
+        updatePost: {},
+        deletePost: {}
+    },
 };
 
 api.init(CoCreateLinkedin);
